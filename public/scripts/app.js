ff3ce9bc2e0c0ac349e75d1e096248b47274584e
Sera Bak <serabak15@gmail.com>
2016-09-06 14:48:04 -0700

    creates database and get route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,56 +6,45 @@
  */
 
 
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
-
 var source;
 var template;
 
 $(document).ready(function() {
   console.log('app.js loaded!');
   source = $('#albums-template').text();
-   template= Handlebars.compile(source);
-  renderAlbum();
+  template= Handlebars.compile(source);
+  //renderAlbum();
+
+  // $.get('/api/albums').success(function (albums) {
+  //   albums.forEach(function(album) {
+  //     renderAlbum(album);
+  //   });
+  // });
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/albums',
+    success: onSuccess,
+    
+  })
+
+
+
 });
 
 
-
-
-
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
+function onSuccess(albums) {
+  renderAlbum(albums);
+}
+
+function renderAlbum(albums) {
   var albumHtml;
-  for (var i=0; i<sampleAlbums.length; i++){
-     albumHtml = template(sampleAlbums[i]);
+  for (var i=0; i<albums.length; i++){
+     albumHtml = template(albums[i]);
      $("#albums").append(albumHtml);
   }
-  console.log('rendering album:', album);
-
+  console.log('rendering album:', albums);
 }
+
+
--- seed.js
+++ seed.js
@@ -4,12 +4,37 @@
 var db = require("./models");
 
 var albumsList =[
-  // put data here!
-  {albumName: "Anti",
-   artistName: "Rihanna",
-   releasedDate: "January 2016"
+  {
+    artistName: 'Nine Inch Nails',
+    name: 'The Downward Spiral',
+    releaseDate: '1994, March 8',
+    genres: [ 'industrial', 'industrial metal' ]
+  },
+  {
+    artistName: 'Metallica',
+    name: 'Metallica',
+    releaseDate: '1991, August 12',
+    genres: [ 'heavy metal' ]
+  },
+  {
+    artistName: 'The Prodigy',
+    name: 'Music for the Jilted Generation',
+    releaseDate: '1994, July 4',
+    genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+  },
+  {
+    artistName: 'Johnny Cash',
+    name: 'Unchained',
+    releaseDate: '1996, November 5',
+    genres: [ 'country', 'rock' ]
   }
 ];
+
+
+
+
+
+
 
 db.Album.remove({}, function(err, albums){
 
